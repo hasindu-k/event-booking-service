2b86aa8a9ac33aae11740ed9b2a97e315d75de60
Hasindu Koshitha <hasindu.learn.online@gmail.com>
2026-03-19 16:43:01 +0530

    update

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res, next) => {
-  console.log("Booking service received:", req.method, req.url);
+  console.log("Booking service received: updated", req.method, req.url);
   next();
 });
 
