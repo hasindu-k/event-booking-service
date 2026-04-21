cc1b935c1d69985b7ebc8dacb0b19a5b7cbec39b
Hasindu Koshitha <hasindu.learn.online@gmail.com>
2026-04-21 19:34:32 +0530

    fix: update service status message to indicate it is updated and running

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
   res.status(200).json({
     service: "Booking Service",
-    status: "Running",
+    status: "Updated and Running",
   });
 });
 
